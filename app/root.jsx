fd7618fd7bb97493f5b43878ee54268191a4fbeb
Marcin Czenko <marcin@everydayproductive.com>
2022-02-01 20:25:35 +0100

    adds support for taiwindcss

--- app/root.jsx
+++ app/root.jsx
@@ -7,8 +7,14 @@ import {
   ScrollRestoration
 } from 'remix'
 
+import styles from './styles/app.css'
+
 export function meta () {
   return { title: 'New Remix App' }
+}
+
+export function links () {
+  return [{ rel: 'stylesheet', href: styles }]
 }
 
 export default function App () {
--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,32 +1,9 @@
 export default function Index () {
   return (
-    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target='_blank'
-            href='https://remix.run/tutorials/blog'
-            rel='noreferrer'
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target='_blank'
-            href='https://remix.run/tutorials/jokes'
-            rel='noreferrer'
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target='_blank' href='https://remix.run/docs' rel='noreferrer'>
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div className='w-screen h-screen bg-blue-900 flex items-center justify-center'>
+      <h1 className='text-6xl text-white'>
+        remix starter with tailwindcss
+      </h1>
     </div>
   )
 }
